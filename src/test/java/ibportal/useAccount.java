ee9d29c346717c9b00f8f814ffb0f4348b7c80f8
abimanyu.r <abimanyu.r@CP-156-CS-55.credopay.com>
2025-11-12 13:05:51 +0530

    Initial commit for automation framework

--- src/test/java/ibportal/useAccount.java
+++ src/test/java/ibportal/useAccount.java
@@ -146,7 +146,7 @@ public class useAccount extends BaseApi {
 		}
 
 		
-		
+		System.out.println("Git update");
 		
 		Assert.assertEquals(response.jsonPath().getString("status"), expectedStatus,
 				"Status mismatch for test case: " + testCase);
