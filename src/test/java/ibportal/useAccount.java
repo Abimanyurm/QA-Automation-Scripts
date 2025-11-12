8c517ade4ce837be585a1c3423e3a208503a32d0
abimanyu.r <abimanyu.r@CP-156-CS-55.credopay.com>
2025-11-12 18:41:30 +0530

    user gfd

--- src/test/java/ibportal/useAccount.java
+++ src/test/java/ibportal/useAccount.java
@@ -146,6 +146,7 @@ public class useAccount extends BaseApi {
 		}
 		
 		System.out.println("88888888888888888888");
+		System.out.println("99999999999999999999");
 
 		Assert.assertEquals(response.jsonPath().getString("status"), expectedStatus,
 				"Status mismatch for test case: " + testCase);
