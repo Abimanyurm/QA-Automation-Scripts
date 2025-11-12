35f054d3fcaf70ed2b53b930c98ba59f4bdcd9d9
abimanyu.r <abimanyu.r@CP-156-CS-55.credopay.com>
2025-11-12 18:28:16 +0530

    user add

--- src/test/java/ibportal/useAccount.java
+++ src/test/java/ibportal/useAccount.java
@@ -144,6 +144,8 @@ public class useAccount extends BaseApi {
 				throw new SkipException("GM ID not found – skipping all remaining tests.");
 			}
 		}
+		
+		System.out.println("88888888888888888888");
 
 		Assert.assertEquals(response.jsonPath().getString("status"), expectedStatus,
 				"Status mismatch for test case: " + testCase);
